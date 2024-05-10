edd10d04519656f372493c6d23fb5eb75f3d01e3
JoanOrtiz10 <ortizjoan404@gmail.com>
2024-05-10 18:00:46 -0500

    directives vue

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue' 
+
+const users=["pepe", "Ana", "Juana","lola"]
+
+const isChecked = ref(true)
+</script>
+
+<template>
+    <ul>
+        <li v-for="(user, index) in users" :key="index">{{index}}{{ user }}</li>
+    </ul>
+    <input type="checkbox" id="check" v-model="isChecked"/>
+    <label for="check">pinchame</label>
+    <div class="bg-blue-400 text-white rounded py-1 px-2  w-40 mx-auto" :class="{'bg-orange-500':isChecked}">
+        Este es un mensaje 
+    </div>
+ <div v-show="isChecked">Hola </div>
+ <div v-if="isChecked"> mundo</div>
+ <div v-else>mateo es gay</div>
+
+
+
+
+
+</template>
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+const todos = ref(['Estudiar bastante'])
+const todo = ref('')
+function add() {
+ console.log(todo);
+    todos.value.unshift(todo.value)
+    todo.value = ''
+
+}
+
+</script>
+<template>
+    <div class="  py-6 px-5  rounded shadow bg-slate-200 max-w-96 mx-auto mt-10">
+        <div>
+            <input v-model="todo" class="w-4/5 outline-none rounded px-2">
+            <button @click="add" class=" w-1/5 bg-green-600 text-white text-center ">
+                +
+
+            </button>
+        </div>
+        <div class="mt-3">
+            <div v-for="(todo, i) in todos" :key="i" class="px-1 rounded mt-4 bg-orange-300">
+                {{ todo }}
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
